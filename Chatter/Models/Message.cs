be31d5f31338ee0762fe79ce2807a611f9026473
ChristopherMBetteley <Christopher.M.Betteley@gmail.com>
2016-10-27 15:24:55 -0400

    first commit

---
+++ Chatter/Models/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chatter.Models
+{
+    public class Message
+    {
+        public int MessageID { get; set; }
+        public string MessageText { get; set; }
+        public DateTime MessageTimeStamp { get; set; }
+
+        public User User { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ Chatter/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chatter.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string UserLastName { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserCity { get; set; }
+
+        public ICollection<Message> Messages { get; set; }
+    }
+}
